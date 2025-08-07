eacdf4408017e81c80853136610d4b204ca394d6
SUUUPERKENTA <minseo00515@naver.com>
2025-08-07 18:54:17 +0900

    buyList.jsp 에서 구매처 조회 화면 구현 및 검색 구현 @RequestMapping Url 변경 구매처 신규 추가 팝업으로 이어지는 URL '/itman/asset/contWriteAssetSupplier.do' -> '/itman/asset/contWriteSupplier.do' '/itman/asset/insertAssetSupplier.do' -> '/itman/asset/insertSupplier.do'

--- src/main/java/egovframework/itman/asset/web/AssetController.java
+++ src/main/java/egovframework/itman/asset/web/AssetController.java
@@ -179,7 +179,7 @@ public class AssetController {
         return "itman/public/html/popup/contWriteItmSupplier";
     }
 
-    @PostMapping("/itman/asset/insertAssetSupplier.do")
+    @PostMapping("/itman/asset/insertSupplier.do")
     public String insetAssetSupplier(SupplierVO  vo, Model model) throws Exception {
         supplierService.insertAssetSupplier(vo);
         model.addAttribute("script", "<script>window.opener.location.reload(); window.close();</script>");
