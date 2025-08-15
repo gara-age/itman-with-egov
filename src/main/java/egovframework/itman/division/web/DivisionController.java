91e48ba51d883570a1a1d159f05010a50eca13e9
SUUUPERKENTA <minseo00515@naver.com>
2025-08-15 19:22:32 +0900

    디버깅

--- src/main/java/egovframework/itman/division/web/DivisionController.java
+++ src/main/java/egovframework/itman/division/web/DivisionController.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
